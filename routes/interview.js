935a129917d69458e11a96b382086f920466322a
gerarmc <gerardomelendezpro@gmail.com>
2024-01-18 13:59:24 -0400

    basic  functions  done

--- routes/interview.js
+++ routes/interview.js
@@ -2,12 +2,20 @@ var express = require('express');
 var router = express.Router();
 
 const Interview = require("../models/Interview")
+const Company = require('../models/Company')
 
 router.post("/", (req, res, next) => {
   Interview.create(req.body)
     .then((createdInterview) => {
-      console.log("Interview created ->", createdInterview);
-      res.status(201).send(createdInterview);
+      Company.findByIdAndUpdate(createdInterview.company, {$push: {interviews: createdInterview._id}}, {new: true})
+      .then((updatedCompany) => {
+        console.log("Interview created ->", updatedCompany, createdInterview);
+        res.status(201).json({createdInterview, updatedCompany});
+      })
+      .catch((error) => {
+        console.error("Error while creating the Interview ->", error);
+        res.status(500).send({ error: "Failed to create the Interview" });
+      })
     })
     .catch((error) => {
       console.error("Error while creating the Interview ->", error);
@@ -18,9 +26,10 @@ router.post("/", (req, res, next) => {
 router.get("/", (req, res, next) => {
     console.log("Hitting get route")
   Interview.find()
+    .populate('user company')
     .then((foundInterviews) => {
       console.log(foundInterviews);
-      res.status(201).send(foundInterviews);
+      res.status(201).json(foundInterviews);
     })
     .catch((err) => {
       console.log(err);
@@ -31,6 +40,7 @@ router.get("/", (req, res, next) => {
 router.get("/:id", (req, res, next) => {
     console.log("Hitting get route")
   Interview.findById(req.params.id)
+    .populate('user company')
     .then((foundInterviews) => {
       console.log(foundInterviews);
       res.status(201).send(foundInterviews);
@@ -45,7 +55,7 @@ router.post("/update/:id", (req, res, next) => {
   Interview.findByIdAndUpdate(req.params.id, req.body, { new: true })
     .then((updatedInterview) => {
       console.log(updatedInterview);
-      res.status(200).send(updatedInterview);
+      res.status(200).json(updatedInterview);
     })
     .catch((err) => {
       console.log(err);
@@ -57,11 +67,11 @@ router.get('/delete/:id', (req, res, next) => {
     Interview.findByIdAndDelete(req.params.id)
     .then((updatedInterview) => {
         console.log(updatedInterview);
-        res.status(200).send(updatedInterview);
+        res.status(200).json(updatedInterview);
       })
       .catch((err) => {
         console.log(err);
-        res.status(500).send(err);
+        res.status(500).json(err);
       })
 });
 
